fe82f1ef439a7e2d878e3a6116410559fece4ba6
Ashley Sigler <ashleysigler13@gmail.com>
2020-11-24 21:54:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-   word =~ /^[aeiou]\w+/i ? true : false
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\b[Uu]n[a-z]*ing\b/)
+  text.scan.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-  text =~ /\b[a-z]{5}$\b/
+
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
